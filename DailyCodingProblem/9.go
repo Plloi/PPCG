307a4b19f06b7796fcb45cf0a8aee6561568404a
Hammill <cr212418@charlesriverlabs.com>
2018-10-30 15:57:31 -0400

    implement sumNonAdjacent

--- DailyCodingProblem/9.go
+++ DailyCodingProblem/9.go
@@ -1,5 +1,24 @@
 package dailycodingproblem
 
 func sumNonAdjacent(arr []int) int {
-	return 0
+	maxi := 0 //max value including previous item in array, doubles as running max value
+	maxe := 0 //max value excluding previous item in array
+	for _, i := range arr {
+		// add item to maxe if it will increase it's value
+		if i > 0 {
+			maxe += i
+		}
+
+		//grab mai for latter, jic
+		tmp := maxi
+		//only update maxi if maxe is larger
+		if maxi < maxe {
+			maxi = maxe
+		}
+
+		//set maxe to old maxi
+		maxe = tmp
+
+	}
+	return maxi
 }
